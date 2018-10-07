5799e92b60653ddb607691a23e70c2746a4731e7
cxad1345 <cxad1345@163.com>
2018-10-07 23:31:48 +0800

    fixed bug

--- core/lib/log.php
+++ core/lib/log.php
@@ -10,21 +10,25 @@ namespace core\lib;
 class log
 {
     static $class;
+
     /****
      * 1、确定日志的存储方式
-     * 
+     *
      * 2、写日志
-     * 
+     *
      */
-    
-    static public function init(){
+
+    static public function init()
+    {
         //确定存储方式；
-        $drive =conf::get('DRIVE','log');
-        $class='\core\lib\drive\log\\'.$drive;
-        self::$class=new $class;
+        $drive = conf::get('DRIVE', 'log');
+        $class = '\core\lib\drive\log\\' . $drive;
+        self::$class = new $class;
 
     }
-    static public function log($name,$file='log'){
-       self::$class->log($name,$file);
+
+    static public function log($name, $file = 'log')
+    {
+        self::$class->log($name, $file);
     }
 }   
